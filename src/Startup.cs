085abf648c8d7271c30fdb96d024598b9e8e39a9
Ivan Strelkov <ivan.strelkov@myworkflow.ru>
2021-10-19 23:34:01 +0700

    Создан проект, добавлена инициализация контейнеров docker.

---
+++ src/Startup.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ozon.MerchandiseService
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+            });
+        }
+    }
+}
