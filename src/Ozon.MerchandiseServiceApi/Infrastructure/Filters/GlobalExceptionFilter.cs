218725d34b17443ce51bc2716158b255c47774ba
Ivan Strelkov <ivan.strelkov@myworkflow.ru>
2021-10-27 18:57:03 +0700

    update

--- src/Ozon.MerchandiseServiceApi/Infrastructure/Filters/GlobalExceptionFilter.cs
+++ src/Ozon.MerchandiseServiceApi/Infrastructure/Filters/GlobalExceptionFilter.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
@@ -11,7 +12,8 @@ namespace Ozon.MerchandiseServiceApi.Infrastructure.Filters
             var resultObject = new
             {
                 ExceptionType = context.Exception.GetType().FullName,
-                Message = context.Exception.Message
+                Message = context.Exception.Message,
+                StackTrace=context.Exception.StackTrace
             };
 
             var jsonResult = new JsonResult(resultObject)
